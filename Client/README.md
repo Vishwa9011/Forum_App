00ca8cf82c6563fc627ab8e0d29ab10a2049b9c5
Vishwa Vivek Yadav <112635764+Vishwa9011@users.noreply.github.com>
2023-02-21 10:42:55 +0530

    dependencies installed

--- Client/README.md
+++ Client/README.md
@@ -5,3 +5,8 @@
 3. chakra-ui
 4. redux react-redux redux-thunk
 5. react-icons
+6. typescript
+
+## to start
+
+1. npm run dev
--- Client/src/main.tsx
+++ Client/src/main.tsx
@@ -1,9 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { ChakraProvider } from '@chakra-ui/react'
 import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Router>
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
+  </Router>
 )
