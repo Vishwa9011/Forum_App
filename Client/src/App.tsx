996cab53d94be72fdbaaf5459855c394ddad17e6
Ashok Kumar <ap271449@gmail.com>
2023-02-22 11:59:11 +0530

    modified : edit user modal

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
+import Navbar from "./Components/Navbar/Navbar";
+import Profile from "./Pages/Profile/Profile";
 
 function App() {
   return (
     <>
-
+      <Navbar />
+      <Profile />
     </>
   );
 }
