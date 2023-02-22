6eaa6c2cbc2743814fb8566680422fc922042df0
Saurav903 <sk8517167@gmail.com>
2023-02-22 18:39:13 +0530

    some changes in login

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,13 +1,15 @@
 
 import React from 'react'
 import Login from './Pages/Auth/Login/Login';
+import Createpost from './Pages/Post/Createpost';
 
 
 
 function App() {
   return (
-    <>
-    
+    <>  
+    <Createpost/>
+    <Login/>
     </>
   );
 }
