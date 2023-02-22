d8689a18c5b67f0fc15ed60af0a1b5dc1a589e6e
Ashok Kumar <ap271449@gmail.com>
2023-02-22 10:45:14 +0530

    arranged

--- Client/src/Redux/auth/signup/signup.api.ts
+++ Client/src/Redux/auth/signup/signup.api.ts
@@ -4,7 +4,7 @@ import { RegisterResI, UserI } from "../../../Constants/Constants";
 
 export const registerUser = async(payload:UserI)=>{
     try {
-        let res:AxiosResponse<RegisterResI> = await axios.post(`${process.env.BASE_URL}/users/register`,payload);
+        let res: RegisterResI = await axios.post(`${process.env.BASE_URL}/users/register`,payload);
         return res.data;
     } catch (err) {
         console.log(err);
