ff346f8faf02377c27bf03f408e6e2a91ebdbcdd
Vishwa Vivek Yadav <112635764+Vishwa9011@users.noreply.github.com>
2023-02-23 10:41:11 +0530

    post & comment system

--- Server/server.js
+++ Server/server.js
@@ -1,10 +1,17 @@
 const express = require("express");
 require("dotenv").config();
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
 const { connection } = require("./Configs/db");
 const { PostRouter } = require("./Routes/post.routes");
 const { UserRouter } = require("./Routes/user.router");
 
 const app = express();
+
+app.use(cookieParser())
+
+
+app.use(cors())
 
 app.use(express.json());
 
@@ -13,6 +20,12 @@ app.use("/user", UserRouter)
 app.use("/post", PostRouter)
 
 app.get("/", (req, res) => {
+     const cookie = req.cookies.User
+     if (cookie == null) {
+          res.cookie('User', "63f631014d5749dd2bf27257", { httpOnly: true });
+     } else {
+          console.log('cookie: ', cookie);
+     }
      res.send("Home Page")
 })
 
