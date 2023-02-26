9b78cac1270614e8dcc1970fd6148397b5114494
Ashok Kumar <ap271449@gmail.com>
2023-02-26 20:39:58 +0530

    fixed : navbar bugs

--- Client/src/Components/Navbar/Navbar.tsx
+++ Client/src/Components/Navbar/Navbar.tsx
@@ -1,6 +1,22 @@
 import { useState, useEffect } from "react";
-import { Box, Flex, HStack, useDisclosure, useColorModeValue, Stack, Container, IconButton, Text, } from "@chakra-ui/react";
-import { HamburgerIcon, CloseIcon, BellIcon, ChatIcon, SearchIcon } from "@chakra-ui/icons";
+import {
+  Box,
+  Flex,
+  HStack,
+  useDisclosure,
+  useColorModeValue,
+  Stack,
+  Container,
+  IconButton,
+  Text,
+} from "@chakra-ui/react";
+import {
+  HamburgerIcon,
+  CloseIcon,
+  BellIcon,
+  ChatIcon,
+  SearchIcon,
+} from "@chakra-ui/icons";
 import { FaHome } from "react-icons/fa";
 import { FiSearch } from "react-icons/fi";
 import UserProfile from "./UserProfile";
@@ -9,75 +25,178 @@ import { useSelector } from "react-redux";
 import { RootState } from "../../Redux/store";
 import useToggle from "../../Custom-Hooks/useToggle";
 import SearchBar from "../Searchbar/SearchBar";
-import './Navbar.css'
+import "./Navbar.css";
 const Links = [
-     {
-          name: "Home",
-          icon: <FaHome />,
-          href: "/",
-     },
-     {
-          name: "Message",
-          icon: <ChatIcon />,
-          href: "mailto:vishu842301@gmail.com",
-     },
-     {
-          name: "Notification",
-          icon: <BellIcon />,
-          href: "/notifications",
-     },
+  {
+    name: "Home",
+    icon: <FaHome />,
+    href: "/",
+  },
+  {
+    name: "Message",
+    icon: <ChatIcon />,
+    href: "mailto:vishu842301@gmail.com",
+  },
+  {
+    name: "Notification",
+    icon: <BellIcon />,
+    href: "/notifications",
+  },
 ];
 
 export default function Navbar() {
-     const [searchParam, setSearchParams] = useSearchParams();
-     const { userCredential } = useSelector((store: RootState) => store.auth);
-     const [isOpen, onOpen, onClose]: any = useToggle(true);
-     const [isOpenSearchbar, openSearch, closeSearch]: any = useToggle(false);
-     return (
-          <>
-               {isOpenSearchbar && <SearchBar toggle={closeSearch} />}
-               <Box px={4} shadow={"md"} pos='sticky' top='0' left='0' zIndex={99} bg='white'>
-                    <Box w='80%' m='auto'>
-                         <Flex h={16} alignItems={"center"} justifyContent={"space-between"}>
-                              <IconButton size={"md"} icon={isOpen ? <CloseIcon /> : <HamburgerIcon />} aria-label={"Open Menu"} display={{ md: "none" }} onClick={isOpen ? onClose : onOpen} />
-                              <HStack spacing={8} alignItems={"center"}>
-                                   <Box className="logo" as={Link} to="/" fontSize={"1.3rem"} letterSpacing='1px' fontWeight="bold" cursor={"pointer"}>
-                                        <Text as='span'>F</Text>
-                                        <Text as='span' color={'red.500'}>O</Text>
-                                        <Text as='span'>R</Text>
-                                        <Text as='span' color={'red.500'}>U</Text>
-                                        <Text as='span'>M</Text>
-                                   </Box>
-                                   <HStack as={"nav"} spacing={4} display={{ base: "none", md: "flex" }}>
-                                        {Links.map((link, i) => (
-                                             <Text fontWeight={'semibold'} letterSpacing='1px' fontSize={'1.1em'} as={Link} key={i} to={`${link.href}`}>
-                                                  {link.name}
-                                             </Text>
-                                        ))}
-                                   </HStack>
-                              </HStack>
-                              <Flex alignItems={"center"} gap='20px'>
-                                   <Flex onClick={openSearch} className="user-select-reject" cursor={'pointer'} border={'1px'} borderRadius='5px' borderColor='gray.300' align={'center'} p='1'>
-                                        <Box flex={1} w='200px' pl='2' color={'gray.600'} fontWeight='semibold'>{searchParam.get('user') || "Search"}</Box>
-                                        <Flex borderLeft={'1px'} p='1' pl='2' align={'center'} fontSize={'1.5rem'}><SearchIcon /></Flex>
-                                   </Flex>
-                                   {userCredential.token ? (
-                                        <UserProfile />
-                                   ) : (
-                                        <Flex gap={2}>
-                                             <Text as={Link} to="/signup">
-                                                  Signup
-                                             </Text>
-                                             <Text>|</Text>
-                                             <Text as={Link} to="/login">
-                                                  Login
-                                             </Text>
-                                        </Flex>
-                                   )}
-                              </Flex>
-                         </Flex>
-                    </Box>
-               </Box>
-          </>
-     );
+  const [searchParam, setSearchParams] = useSearchParams();
+  const { userCredential } = useSelector((store: RootState) => store.auth);
+  const [isOpen, onOpen, onClose]: any = useToggle(true);
+  const [isOpenSearchbar, openSearch, closeSearch]: any = useToggle(false);
+  return (
+    <>
+      {isOpenSearchbar && <SearchBar toggle={closeSearch} />}
+      <Box
+        px={4}
+        shadow={"md"}
+        pos="sticky"
+        top="0"
+        left="0"
+        zIndex={99}
+        bg="white"
+      >
+        <Container maxW={"5xl"}>
+          <Flex h={16} alignItems={"center"} justifyContent={"space-between"}>
+            <IconButton
+              size={"md"}
+              icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
+              aria-label={"Open Menu"}
+              display={{ md: "none" }}
+              onClick={isOpen ? onClose : onOpen}
+            />
+            <HStack spacing={8} alignItems={"center"}>
+              <Box
+                className="logo"
+                as={Link}
+                to="/"
+                fontSize={"1.3rem"}
+                letterSpacing="1px"
+                fontWeight="bold"
+                cursor={"pointer"}
+              >
+                <Text as="span">F</Text>
+                <Text as="span" color={"red.500"}>
+                  O
+                </Text>
+                <Text as="span">R</Text>
+                <Text as="span" color={"red.500"}>
+                  U
+                </Text>
+                <Text as="span">M</Text>
+              </Box>
+              <HStack
+                as={"nav"}
+                spacing={4}
+                display={{ base: "none", md: "flex" }}
+              >
+                {Links.map((link, i) => (
+                  <Text
+                    fontWeight={"semibold"}
+                    letterSpacing="1px"
+                    fontSize={"1.1em"}
+                    as={Link}
+                    key={i}
+                    to={`${link.href}`}
+                  >
+                    {link.name}
+                  </Text>
+                ))}
+              </HStack>
+            </HStack>
+            <Flex alignItems={"center"} gap="20px">
+              <Flex
+                display={{ base: "none", md: "flex" }}
+                onClick={openSearch}
+                className="user-select-reject"
+                cursor={"pointer"}
+                border={"1px"}
+                borderRadius="5px"
+                borderColor="gray.300"
+                align={"center"}
+                p="1"
+              >
+                <Box
+                  flex={1}
+                  w="200px"
+                  pl="2"
+                  color={"gray.600"}
+                  fontWeight="semibold"
+                >
+                  {searchParam.get("user") || "Search"}
+                </Box>
+                <Flex
+                  borderLeft={"1px"}
+                  p="1"
+                  pl="2"
+                  align={"center"}
+                  fontSize={"1.5rem"}
+                >
+                  <SearchIcon />
+                </Flex>
+              </Flex>
+              {userCredential.token ? (
+                <UserProfile />
+              ) : (
+                <Flex gap={2}>
+                  <Text as={Link} to="/signup">
+                    Signup
+                  </Text>
+                  <Text>|</Text>
+                  <Text as={Link} to="/login">
+                    Login
+                  </Text>
+                </Flex>
+              )}
+            </Flex>
+          </Flex>
+          {isOpen ? (
+            <Box pb={4} display={{ md: "none" }}>
+              <Stack as={"nav"} spacing={4}>
+                {Links.map((link, i) => (
+                  <Link key={i} to={`${link.href}`}>
+                    {link.name}
+                  </Link>
+                ))}
+              </Stack>
+              <Flex
+                onClick={openSearch}
+                className="user-select-reject"
+                cursor={"pointer"}
+                border={"1px"}
+                borderRadius="5px"
+                borderColor="gray.300"
+                align={"center"}
+                p="1"
+              >
+                <Box
+                  flex={1}
+                  w="200px"
+                  pl="2"
+                  color={"gray.600"}
+                  fontWeight="semibold"
+                >
+                  {searchParam.get("user") || "Search"}
+                </Box>
+                <Flex
+                  borderLeft={"1px"}
+                  p="1"
+                  pl="2"
+                  align={"center"}
+                  fontSize={"1.5rem"}
+                >
+                  <SearchIcon />
+                </Flex>
+              </Flex>
+            </Box>
+          ) : null}
+        </Container>
+      </Box>
+    </>
+  );
 }
--- Client/src/Pages/Home/Home.tsx
+++ Client/src/Pages/Home/Home.tsx
@@ -5,24 +5,31 @@ import LeftCompo from "./LeftCompo";
 import RightCompo from "./RightCompo";
 import Post from "../Post/Post";
 
-
 type Props = {};
 
-function Home({ }: Props) {
-	return (
-		<>
-			<Navbar />
-			<Box w="88%" h='120vh' m="auto" display="flex" gap="5">
-				<Box w='25%' display={{ base: "none", sm: "none", md: "none", lg: "block" }}>
-					<LeftCompo />
-				</Box>
-				<Box flex={1} h='100%' overflowY={'scroll'} className='scroll-hidden'><Post /></Box>
-				<Box w='25%' display={{ base: "none", sm: "none", md: "none", lg: "block" }}>
-					<RightCompo />
-				</Box>
-			</Box>
-		</>
-	);
+function Home({}: Props) {
+  return (
+    <>
+      <Navbar />
+      <Box w="88%" h="120vh" m="auto" display="flex" gap="5">
+        <Box
+          w="25%"
+          display={{ base: "none", sm: "none", md: "none", lg: "block" }}
+        >
+          <LeftCompo />
+        </Box>
+        <Box flex={1} h="100%" overflowY={"scroll"} className="scroll-hidden">
+          <Post />
+        </Box>
+        <Box
+          w="25%"
+          display={{ base: "none", sm: "none", md: "none", lg: "block" }}
+        >
+          <RightCompo />
+        </Box>
+      </Box>
+    </>
+  );
 }
 
 export default Home;
