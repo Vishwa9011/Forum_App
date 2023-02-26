01ff12b7e21c8e4a64b486a08cd2ab805ae65695
Saurav903 <sk8517167@gmail.com>
2023-02-26 17:01:50 +0530

    follower page

--- Client/src/Application.tsx
+++ Client/src/Application.tsx
@@ -1,5 +1,6 @@
 import React, { lazy } from "react";
 import { Route, Routes } from "react-router-dom";
+import Follower from "./Pages/Follower/Follower";
 import RequiredRoute from "./RequiredRoute";
 const Home = lazy(() => import("./Pages/Home/Home"));
 const Login = lazy(() => import("./Pages/Auth/Login/Login"));
@@ -25,6 +26,7 @@ function Application() {
       <Route path="/sendverifyemail" element={<RequiredRoute><SendVerificationEmail /></RequiredRoute>} />
       <Route path="/verifyemail" element={<RequiredRoute><VerifyEmail /></RequiredRoute>} />
       <Route path="/profile/edit" element={<RequiredRoute><></></RequiredRoute>} />
+      <Route path="/follower" element={<Follower/>} />
     </Routes>
   );
 }
